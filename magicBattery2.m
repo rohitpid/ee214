a01627b055dd98baf51249e184ffbc724c2443ef
Rohit Pidaparthi <rohitpid@gmail.com>
2012-11-19 22:48:44 -0800

    add magicBattery2.m which has calculations to make magic battery

---
+++ magicBattery2.m
@@ -0,0 +1,11 @@
+% You give me a Vb you want
+Vb = 2;
+k = 3; % you give me a device scaling parameter
+Vt = 0.5;
+W = 6;
+L = 1;
+KP = 50e-6;
+
+Ibias = ((Vb-Vt)^2*(W/L)*KP)/((k+1)*2)
+
+R = 5/Ibias
