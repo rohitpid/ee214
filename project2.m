d5a34182307cb58bf78f86160dc6a1a5cca1acf7
Rohit Pidaparthi <rohitpid@gmail.com>
2012-11-19 18:25:08 -0800

    Updated project2.m to include looping over Vov and incorporated gain check. Also use Miller instead of ZVTC for some caps.

--- project2.m
+++ project2.m
@@ -43,6 +43,7 @@ Vov1 = 0.3;
 Vov2 = 0.3;
 Vov3 = 0.3;
 VovL1 = 0.3;
+VovL2 = 0.3;
 
 %%%%%%%%%%%%%% Optimization Parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -52,94 +53,138 @@ Av1 = 1000;           % Gain of 1st stage
 
 %%%%%%%%%%%%%%%%%%%%%%%%% Dependent Params %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Av2 = Rm / .8 / Av1;% Gain of 2nd stage
+%Av2 = Rm / .8 / Av1;% Gain of 2nd stage
 
 feasible_ID = [];
 feasible_W = [];
-minW = 1e-6*[1 1 1 1 1];
-sweep = 1:100;
+feasible_gain = [];
+feasible_BW = [];
+gbp = [];
+minW = 2e-6*[1 1 1 1 1];
+%sweep = 1:100;
 
-ID1_IDtot = linspace(.05, .9, 100);
-ID2_IDtot = linspace(.05, .9, 100);
+ID1_IDtot = linspace(.05, .9, 20);
+ID2_IDtot = linspace(.05, .9, 20);
 
-for i = 1 : length(ID1_IDtot)
-    for j = 1 : length(ID2_IDtot)
-        ID1 = ID1_IDtot(i) * IDtot;
-        ID2 = ID2_IDtot(j) * IDtot;
-        ID3 = (1 - ID1_IDtot(i) - ID2_IDtot(j)) * IDtot;
-        
-        if ID1_IDtot(i) + ID2_IDtot(j) > 1
-            break;
-        end
-        
-        %%%% Calculations for Tau1
-        % M1
-        gm1 = 2*ID1/Vov1;
-        w1 = 2*ID1*L1 / (kp_n*Vov1^2);
-        Cgs1 = 2/3*w1*L1*Cox;
-        Csb1 = Cgs1*Csb_Cgs;
-        Cgd1 = Cgs1*Cgd_Cgs;
-        Cdb1 = Cgs1*Cdb_Cgs;
-        
-        tau1 = (1/gm1)*(Cgs1+Cin+Csb1);
-        
-        %%% Calculations for Tau2
-        % ML1
-%         gmL1 = 2*ID1/VovL1;
-        gmL1 = Av1 * ID1 / (Av1 * Vdd - 2 * ID1);
-        wL1 = 2*ID1*LL1 / (kp_p*VovL1^2);
-        CgsL1 = 2/3*wL1*LL1*Cox;
-        CdbL1 = CgsL1*Cdb_Cgs;
+Vov1Vec = linspace(0.15,1.2,10);
+Vov2Vec = linspace(0.15,1.2,10);
+Vov3Vec = linspace(0.15,1.2,10);
+VovL1Vec = linspace(0.15,1.2,10);
+VovL2Vec = linspace(0.15,1.2,10);
 
-        VovL2 = Vov2 * gmL1 * (Vdd - 1/gmL1 * ID1 * Av2) / ID1
-        
-        % M2
-        gm2 = 2*ID2/Vov2;
-        w2 = 2*ID2*L2 / (kp_n*Vov2^2);
-        Cgs2 = 2/3*w2*L2*Cox;
-        Csb2 = Cgs2*Csb_Cgs;
-        Cgd2 = Cgs2*Cgd_Cgs;
-        Cdb2 = Cgs2*Cdb_Cgs;
-        tau2 = (1/gmL1)*(Cgd1+Cdb1+CgsL1+Cgs2+CdbL1+Cgd2) + (Rm/0.8)*Cgd2;
-        
-        %%% Calculations for Tau3
-        %ML2
-        gmL2  = 2*ID2/VovL2;
-        wL2   = 2*ID2*LL2 / (kp_p*VovL2^2);
-        CgsL2 = 2/3*wL2*LL2*Cox;
-        CdbL2 = CgsL2*Cdb_Cgs;
-        
-        % M3
-        gm3  = 2*ID3/Vov3;
-        w3   = 2*ID3*L3 / (kp_n*Vov3^2);
-        Cgs3 = 2/3*w3*L3*Cox;
-        Csb3 = Cgs3*Csb_Cgs;
-        Cgd3 = Cgs3*Cgd_Cgs;
-        Cdb3 = Cgs3*Cdb_Cgs;
-        gmb3 = gm3*0.2;
-        gm3prime = gm3+gmb3;
-        
-        tau3 = (1/gmL2) * (Cgd2+Cgd3+Cdb2+CgsL2+CdbL2+ (1/(1+(gm3/gmb3)))*Cgs3);
-        
-        % Calculations for tau4
-        
-        tau4 = (Rout / (Rout * gm3prime + 1)) * (Cout+Csb3+Cgs3);
-        
-        tau = [tau1 tau2 tau3 tau4];
-        
-        f3dB = 1/(2*pi) * 1/sum(tau);
-        gain = 1/gmL1 * gm2 / gmL2 * 0.8
-        
-        IDVec = [ID1 ID2 ID3];
-        Wvec = [w1 w2 w3 wL1 wL2];
-        
-        if(f3dB > f3dB_target && sum(Wvec > minW) == 5 && sum(IDVec*5) > 2.4e-3 && gain >= Rm)
-            disp('feasible point found');
-            feasible_ID = [feasible_ID ; IDVec];
-            disp(['i=' num2str(i) ' ' num2str(ID1_IDtot(i)) ' ' num2str(ID1)]);
-            disp(['j=' num2str(j) ' ' num2str(ID2_IDtot(j)) ' ' num2str(ID2)]);
-            feasible_W = [feasible_W; Wvec];
+for k=1:length(Vov1Vec)
+    Vov1 = Vov1Vec(k);
+    for l=1:length(Vov2Vec)
+        Vov2 = Vov2Vec(l);
+        for m=1:length(Vov3Vec)
+            Vov3 = Vov3Vec(m);
+            for n=1:length(VovL1Vec)
+                VovL1 = VovL1Vec(n);
+                for o=1:length(VovL2Vec)
+                    VovL2 = VovL2Vec(o);
+                    for i = 1 : length(ID1_IDtot)
+                        ID1 = ID1_IDtot(i) * IDtot;
+                        for j = 1 : length(ID2_IDtot)
+                            ID2 = ID2_IDtot(j) * IDtot;
+                            ID3 = (1 - ID1_IDtot(i) - ID2_IDtot(j)) * IDtot;
+                            
+                            if ID1_IDtot(i) + ID2_IDtot(j) > 1
+                                break;
+                            end
+                            
+                            %%%% Calculations for Tau1
+                            % M1
+                            gm1 = 2*ID1/Vov1;
+                            w1 = 2*ID1*L1 / (kp_n*Vov1^2);
+                            Cgs1 = 2/3*w1*L1*Cox;
+                            Csb1 = Cgs1*Csb_Cgs;
+                            Cgd1 = Cgs1*Cgd_Cgs;
+                            Cdb1 = Cgs1*Cdb_Cgs;
+                            
+                            tau1 = (1/gm1)*(Cgs1+Cin+Csb1); %Both ZVTC and Miller
+                            
+                            %%% Calculations for Tau2
+                            % ML1
+                            gmL1 = 2*ID1/VovL1;
+                            %        gmL1 = Av1 * ID1 / (Av1 * Vdd - 2 * ID1);
+                            wL1 = 2*ID1*LL1 / (kp_p*VovL1^2);
+                            CgsL1 = 2/3*wL1*LL1*Cox;
+                            CdbL1 = CgsL1*Cdb_Cgs;
+                            
+                            %        VovL2 = Vov2 * gmL1 * (Vdd - 1/gmL1 * ID1 * Av2) / ID1
+                            
+                            %ML2
+                            gmL2  = 2*ID2/VovL2;
+                            wL2   = 2*ID2*LL2 / (kp_p*VovL2^2);
+                            CgsL2 = 2/3*wL2*LL2*Cox;
+                            CdbL2 = CgsL2*Cdb_Cgs;
+                            
+                            % M2
+                            gm2 = 2*ID2/Vov2;
+                            w2 = 2*ID2*L2 / (kp_n*Vov2^2);
+                            Cgs2 = 2/3*w2*L2*Cox;
+                            Csb2 = Cgs2*Csb_Cgs;
+                            Cgd2 = Cgs2*Cgd_Cgs;
+                            Cdb2 = Cgs2*Cdb_Cgs;
+                            
+                            %tau2 =
+                            %(1/gmL1)*(Cgd1+Cdb1+CgsL1+Cgs2+CdbL1+Cgd2 + (gm2/gmL2)*Cgd2); % ZVTC
+                            
+                            tau2 = (1/gmL1)*(Cgd1+Cdb1+CgsL1+Cgs2+CdbL1 + (1+gm2/gmL2)*Cgd2); %MIller
+                            
+                            %%% Calculations for Tau3
+                            
+                            % M3
+                            gm3  = 2*ID3/Vov3;
+                            w3   = 2*ID3*L3 / (kp_n*Vov3^2);
+                            Cgs3 = 2/3*w3*L3*Cox;
+                            Csb3 = Cgs3*Csb_Cgs;
+                            Cgd3 = Cgs3*Cgd_Cgs;
+                            Cdb3 = Cgs3*Cdb_Cgs;
+                            gmb3 = gm3*0.2;
+                            gm3prime = gm3+gmb3;
+                            
+                            %tau3 = (1/gmL2) * (Cgd2+Cgd3+Cdb2+CgsL2+CdbL2 + (1/(1+(gm3/gmb3)))*Cgs3); ZVTC 
+                            tau3 = (1/gmL2) * (Cgd3+Cdb2+CgsL2+CdbL2+(1-0.8)*Cgs3); % Miller
+                            
+                            % Calculations for tau4
+                            
+                            %tau4 = (Rout / (Rout * gm3prime + 1)) * (Cout+Csb3+Cgs3); %ZVTC
+                            tau4 = (Rout / (Rout * gm3prime + 1)) * (Cout+Csb3); %Miller
+                            
+                            tau = [tau1 tau2 tau3 tau4];
+                            
+                            f3dB = 1/(2*pi) * 1/sum(tau);
+                            %gain = 1/gmL1 * gm2 / gmL2 * 0.8
+                            
+                            Av1 = 1/gmL1;
+                            Av2 = gm2/gmL2;
+                            Av3 = gm3/(gm3+gmb3);
+                            
+                            gain = Av1*Av2*Av3;
+                            
+                            IDVec = [ID1 ID2 ID3];
+                            Wvec = [w1 w2 w3 wL1 wL2];
+                            if(f3dB > f3dB_target && sum(Wvec > minW) == 5 && gain >= Rm)
+                                disp('feasible point found');
+                                feasible_ID = [feasible_ID ; IDVec];
+                                %disp(['i=' num2str(i) ' ' num2str(ID1_IDtot(i)) ' ' num2str(ID1)]);
+                                %disp(['j=' num2str(j) ' ' num2str(ID2_IDtot(j)) ' ' num2str(ID2)]);
+                                feasible_W = [feasible_W; Wvec];
+                                feasible_BW = [feasible_BW f3dB];
+                                feasible_gain = [feasible_gain gain];
+                                gbp = [gbp f3dB*gain];
+                                disp(['Gain = ' num2str(gain)]);
+                                disp(['BW = ' num2str(f3dB)]);
+                                disp(['IDs are: ' num2str(IDVec) ]);
+                                disp(['Ws are: ' num2str(Wvec) ]);
+                            end
+                        end
+                    end
+                end
+            end
         end
     end
 end
 
+[maxgbp mgbpidx] = max(gbp)
