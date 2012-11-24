568fd0433b723a087fe8596f17d22ba349ee7410
Rohit Pidaparthi <rohitpid@gmail.com>
2012-11-24 02:27:48 -0800

    Magic battery script updated

--- magicBattery2.m
+++ magicBattery2.m
@@ -1,11 +1,15 @@
+clc;
+clear;
 % You give me a Vb you want
-Vb = 1.65;
-k = 3; % you give me a device scaling parameter
+Vdd = 5;
+Vb = 1.5;
+k = [1:20]'; % you give me a device scaling parameter
 Vt = 0.5;
-W = 6;
+Wmin = 2;
+W = Wmin*k;
 L = 1;
 KP = 50e-6;
 
-Ibias = ((Vb-Vt)^2*(W/L)*KP)/((k+1)*2)
+Ibias = ((Vb-Vt)^2*(W ./ L)*KP) ./ ((k+1)*2)
 
-R = 5/Ibias
+R = (Vdd-Vb) ./ Ibias
